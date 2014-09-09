fbb7e3e2c0180d19cd3361680865ad783d9641af
Merrifield, Jay <jmerrifiel@gannett.com>
2014-09-08 23:20:15 -0400

    fixing package files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
             '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
             '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
             '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed <%= props.license %> */\n',
+            ' Licensed <%= pkg.license %> */\n',
         // Task configuration
         concat: {
             options: {
